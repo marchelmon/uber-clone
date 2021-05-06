253a62ee6b825f9d935f6d89263c36f3436754c8
Marc Hulterström <marc.hulterstrom@gmail.com>
2021-05-06 12:08:33 +0200

    Creating Xcode project

---
+++ uber-clone/uber-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uber-clone
+//
+//  Created by marchelmon on 2021-05-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
