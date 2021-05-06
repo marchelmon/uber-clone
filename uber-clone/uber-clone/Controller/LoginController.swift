aa301c5c792982d2b1f536f48d283c5e8cdbd890
Marc Hulterström <marc.hulterstrom@gmail.com>
2021-05-06 12:23:49 +0200

    Setting up folder structure and changing root view controller to LoginController

--- uber-clone/uber-clone/Controller/LoginController.swift
+++ uber-clone/uber-clone/Controller/LoginController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  uber-clone
 //
 //  Created by marchelmon on 2021-05-06.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
-
-
+    
 }
-
