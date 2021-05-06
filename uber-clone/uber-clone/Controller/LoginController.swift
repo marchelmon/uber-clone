bb4239621362afd571b36e9b5a6c1ff8b47f2874
Marc Hulterström <marc.hulterstrom@gmail.com>
2021-05-06 19:03:12 +0200

    Adding title logo to LoginController

--- uber-clone/uber-clone/Controller/LoginController.swift
+++ uber-clone/uber-clone/Controller/LoginController.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class LoginController: UIViewController {
     
+    //MARK: - Properties
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+    
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
         
-        view.backgroundColor = .red
+        view.addSubview(titleLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
     
 }
