ed5aea5e33138b59d794f0c2cc9a38ce0d147584
Marc Hulterström <marc.hulterstrom@gmail.com>
2021-05-12 09:25:32 +0200

    Adding Textfield and UI-elements to LocationInputView

--- uber-clone/uber-clone/View/LocationInputView.swift
+++ uber-clone/uber-clone/View/LocationInputView.swift
@@ -24,6 +24,67 @@ class LocationInputView: UIView {
         return button
     }()
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Username"
+        label.font = UIFont(name: "Avenir-Light", size: 16)
+        label.textColor = .darkGray
+        return label
+    }()
+    
+    private let startLocationIndicatorView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .lightGray
+        view.setDimensions(width: 6, height: 6)
+        view.layer.cornerRadius = 6/2
+        return view
+    }()
+    
+    private let linkingView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .darkGray
+        return view
+    }()
+    
+    private let destinationIndicatorView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .black
+        view.setDimensions(width: 6, height: 6)
+        return view
+    }()
+    
+    private lazy var startingLocationTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Current location"
+        tf.backgroundColor = .systemGroupedBackground
+        tf.isEnabled = false
+        tf.font = UIFont.systemFont(ofSize: 14)
+        
+        let paddingView = UIView()
+        paddingView.setDimensions(width: 8, height: 30)
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
+        
+        return tf
+    } ()
+    
+    private lazy var destinationTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter destination"
+        tf.backgroundColor = .lightGray
+        tf.returnKeyType = .search
+        tf.font = UIFont.systemFont(ofSize: 14)
+        
+        let paddingView = UIView()
+        paddingView.setDimensions(width: 8, height: 30)
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
+        
+        return tf
+    } ()
+    
+    
+    //MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -33,6 +94,26 @@ class LocationInputView: UIView {
         
         addSubview(backButton)
         backButton.anchor(top: topAnchor, left: leftAnchor, paddingTop: 44, paddingLeft: 16, width: 24, height: 24)
+        
+        addSubview(titleLabel)
+        titleLabel.centerY(inView: backButton)
+        titleLabel.centerX(inView: self)
+        
+        addSubview(startingLocationTextField)
+        startingLocationTextField.anchor(top: backButton.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 40, paddingRight: 40, height: 30)
+        
+        addSubview(destinationTextField)
+        destinationTextField.anchor(top: startingLocationTextField.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 12, paddingLeft: 40, paddingRight: 40, height: 30)
+        
+        addSubview(startLocationIndicatorView)
+        startLocationIndicatorView.centerY(inView: startingLocationTextField, leftAnchor: leftAnchor, paddingLeft: 20)
+
+        addSubview(destinationIndicatorView)
+        destinationIndicatorView.centerY(inView: destinationTextField, leftAnchor: leftAnchor, paddingLeft: 20)
+        
+        addSubview(linkingView)
+        linkingView.centerX(inView: startLocationIndicatorView)
+        linkingView.anchor(top: startLocationIndicatorView.bottomAnchor, bottom: destinationIndicatorView.topAnchor, paddingTop: 4, paddingBottom: 4, width: 0.5)
         
     }
     
